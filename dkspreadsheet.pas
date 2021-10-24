6ec5a84c53f0f588cba8005acecd1e3e13f2c85b
Dmitriy Kornilov <dakor2017@yandex.ru>
2021-10-24 17:37:48 +0300

    added dk_sheetutils.pas

---
+++ dkspreadsheet.pas
@@ -0,0 +1,22 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit DKSpreadSheet;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  DK_SheetConst, DK_SheetExporter, DK_SheetWriter, DK_SheetUtils, 
+  LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('DKSpreadSheet', @Register);
+end.
