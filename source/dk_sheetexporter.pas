a07fb38ebddc52d25c7d65a6dc9fb2cdf0fa2a7f
Dmitriy Kornilov <dakor2017@yandex.ru>
2021-11-21 12:27:37 +0300

    Add TGridExporter

--- source/dk_sheetexporter.pas
+++ source/dk_sheetexporter.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, DK_Dialogs, DK_StrUtils, DK_SheetConst, Dialogs,
-  fpstypes, fpspreadsheet, fpsallformats;
+  fpstypes, fpspreadsheet, fpspreadsheetgrid, fpsallformats;
 
 type
 
@@ -14,15 +14,47 @@ type
               pfWidth,   //заполнить по ширине 1 страницы (по высоте - сколько нужно, чтобы вместить все)
               pfHeight); //заполнить по высоте 1 страницы (по ширине - сколько нужно, чтобы вместить все)
 
+  { TGridExporter }
+
+  TGridExporter = class (TObject)
+  private
+      FWorkbook: TsWorkbook;
+      procedure SetSheetName(const AName: String);
+      function GetSheetName: String;
+  public
+      constructor Create(const AGrid: TsWorksheetGrid);
+      destructor  Destroy; override;
+
+      //Direct save to FileName
+      procedure SaveToXLSX(const ADoneMessage: String = '';
+                           const AFileName: String = '';
+                           const AOverwriteExistingFile: Boolean = False);
+      procedure SaveToODS(const ADoneMessage: String = '';
+                           const AFileName: String = '';
+                           const AOverwriteExistingFile: Boolean = False);
+      //Save with SaveDialog
+      procedure Save(const ADoneMessage: String = '';
+                     const ADefaultFileName: String = '';
+                     const AOverwriteExistingFile: Boolean = False);
+
+      procedure PageSettings(const AOrient: TsPageOrientation = spoPortrait;
+                             const APageFit: TPageFit = pfWidth;
+                             const AShowHeaders: Boolean = True;
+                             const AShowGridLines: Boolean = True;
+                             const ATopMargin: Double=10;     //mm
+                             const ALeftMargin: Double=10;    //mm
+                             const ARightMargin: Double=10;   //mm
+                             const ABottomMargin: Double=10;  //mm
+                             const AFooterMargin: Double=0;   //mm
+                             const AHeaderMargin: Double=0);  //mm
+      property SheetName: String read GetSheetName write SetSheetName;
+  end;
+
   { TSheetExporter }
 
   TSheetExporter = class (TObject)
     private
       FWorkbook: TsWorkbook;
-      procedure SaveToFormat(const AFormat: TsSpreadsheetFormat;
-                           const ADoneMessage: String = '';
-                           const AFileName: String = '';
-                           const AOverwriteExistingFile: Boolean = False);
     public
       constructor Create;
       destructor  Destroy; override;
@@ -89,6 +121,140 @@ begin
   FreeAndNil(SD);
 end;
 
+procedure SaveToFormat(const AWorkbook: TsWorkbook;
+                       const AFormat: TsSpreadsheetFormat;
+                       const ADoneMessage: String = '';
+                       const AFileName: String = '';
+                       const AOverwriteExistingFile: Boolean = False);
+begin
+  if not AOverwriteExistingFile then
+    if FileExists(AFileName) then
+      if not Confirm('Файл "' + AFileName +
+                     '" уже существует! Перезаписать файл?') then Exit;
+  AWorkbook.WriteToFile(AFileName, AFormat, True);
+  if ADoneMessage<>EmptyStr then ShowInfo(ADoneMessage);
+end;
+
+procedure SaveWithDialog(const AWorkbook: TsWorkbook;
+                         const ADoneMessage: String;
+                         const ADefaultFileName: String;
+                         const AOverwriteExistingFile: Boolean = False);
+var
+  FileFormat: TsSpreadsheetFormat;
+  FileName: String;
+begin
+  FileName:=  ADefaultFileName;
+  if not OpenSaveDialog(FileName, FileFormat) then Exit;
+  SaveToFormat(AWorkbook, FileFormat, ADoneMessage,
+               FileName, AOverwriteExistingFile);
+end;
+
+procedure SheetPageSettings(const ASheet: TsWorksheet;
+                       const AOrient: TsPageOrientation = spoPortrait;
+                       const APageFit: TPageFit = pfWidth;
+                       const AShowHeaders: Boolean = True;
+                       const AShowGridLines: Boolean = True;
+                       const ATopMargin: Double=10;     //mm
+                       const ALeftMargin: Double=10;    //mm
+                       const ARightMargin: Double=10;   //mm
+                       const ABottomMargin: Double=10;  //mm
+                       const AFooterMargin: Double=0;   //mm
+                       const AHeaderMargin: Double=0);  //mm
+begin
+  ASheet.Options:= [];
+  if AShowHeaders then
+    ASheet.Options:= ASheet.Options + [soShowHeaders];
+  if AShowGridLines then
+    ASheet.Options:= ASheet.Options + [soShowGridLines];
+  ASheet.PageLayout.Orientation:= AOrient;
+  ASheet.PageLayout.TopMargin:= ATopMargin;
+  ASheet.PageLayout.LeftMargin:= ALeftMargin;
+  ASheet.PageLayout.RightMargin:= ARightMargin;
+  ASheet.PageLayout.BottomMargin:= ABottomMargin;
+  ASheet.PageLayout.HeaderMargin:= AHeaderMargin;
+  ASheet.PageLayout.FooterMargin:= AFooterMargin;
+  ASheet.PageLayout.Options:=
+    ASheet.PageLayout.Options + [poFitPages, poHorCentered];
+  case APageFit of
+    pfOnePage: begin
+                 ASheet.PageLayout.FitWidthToPages:= 1;
+                 ASheet.PageLayout.FitHeightToPages:= 1;
+               end;
+    pfWidth  : begin
+                 ASheet.PageLayout.FitWidthToPages:= 1;
+                 ASheet.PageLayout.FitHeightToPages:= 0;
+               end;
+    pfHeight : begin
+                 ASheet.PageLayout.FitWidthToPages:= 0;
+                 ASheet.PageLayout.FitHeightToPages:= 1;
+               end;
+  end;
+end;
+
+{ TGridExporter }
+
+constructor TGridExporter.Create(const AGrid: TsWorksheetGrid);
+begin
+  FWorkbook:= AGrid.Workbook;
+  PageSettings;
+end;
+
+destructor TGridExporter.Destroy;
+begin
+  inherited Destroy;
+end;
+
+procedure TGridExporter.SetSheetName(const AName: String);
+begin
+  FWorkbook.ActiveWorksheet.Name:= TextToSheetName(AName);
+end;
+
+function TGridExporter.GetSheetName: String;
+begin
+  Result:= FWorkbook.ActiveWorksheet.Name;
+end;
+
+procedure TGridExporter.SaveToXLSX(const ADoneMessage: String;
+  const AFileName: String; const AOverwriteExistingFile: Boolean);
+begin
+  SaveToFormat(FWorkbook, sfOOXML, ADoneMessage,
+               AFileName, AOverwriteExistingFile);
+end;
+
+procedure TGridExporter.SaveToODS(const ADoneMessage: String;
+  const AFileName: String; const AOverwriteExistingFile: Boolean);
+begin
+  SaveToFormat(FWorkbook, sfOpenDocument, ADoneMessage,
+               AFileName, AOverwriteExistingFile);
+end;
+
+procedure TGridExporter.Save(const ADoneMessage: String;
+                              const ADefaultFileName: String;
+                              const AOverwriteExistingFile: Boolean = False);
+begin
+  SaveWithDialog(FWorkbook, ADoneMessage,
+                 ADefaultFileName, AOverwriteExistingFile);
+end;
+
+procedure TGridExporter.PageSettings(
+                             const AOrient: TsPageOrientation = spoPortrait;
+                             const APageFit: TPageFit = pfWidth;
+                             const AShowHeaders: Boolean = True;
+                             const AShowGridLines: Boolean = True;
+                             const ATopMargin: Double=10;     //mm
+                             const ALeftMargin: Double=10;    //mm
+                             const ARightMargin: Double=10;   //mm
+                             const ABottomMargin: Double=10;  //mm
+                             const AFooterMargin: Double=0;   //mm
+                             const AHeaderMargin: Double=0);  //mm
+begin
+  SheetPageSettings(FWorkBook.ActiveWorksheet, AOrient, APageFit,
+              AShowHeaders, AShowGridLines,
+              ATopMargin, ALeftMargin, ARightMargin, ABottomMargin,
+              AFooterMargin, AHeaderMargin);
+
+end;
+
 { TSheetExporter }
 
 constructor TSheetExporter.Create;
@@ -109,41 +275,26 @@ begin
   PageSettings;
 end;
 
-procedure TSheetExporter.SaveToFormat(const AFormat: TsSpreadsheetFormat;
-                           const ADoneMessage: String = '';
-                           const AFileName: String = '';
-                           const AOverwriteExistingFile: Boolean = False);
-begin
-  if not AOverwriteExistingFile then
-    if FileExists(AFileName) then
-      if not Confirm('Файл "' + AFileName +
-                     '" уже существует! Перезаписать файл?') then Exit;
-  FWorkbook.WriteToFile(AFileName, AFormat, True);
-  if ADoneMessage<>EmptyStr then ShowInfo(ADoneMessage);
-end;
-
 procedure TSheetExporter.SaveToXLSX(const ADoneMessage: String;
   const AFileName: String; const AOverwriteExistingFile: Boolean);
 begin
-  SaveToFormat(sfOOXML, ADoneMessage, AFileName, AOverwriteExistingFile);
+  SaveToFormat(FWorkbook, sfOOXML, ADoneMessage,
+               AFileName, AOverwriteExistingFile);
 end;
 
 procedure TSheetExporter.SaveToODS(const ADoneMessage: String;
   const AFileName: String; const AOverwriteExistingFile: Boolean);
 begin
-  SaveToFormat(sfOpenDocument, ADoneMessage, AFileName, AOverwriteExistingFile);
+  SaveToFormat(FWorkbook, sfOpenDocument, ADoneMessage,
+               AFileName, AOverwriteExistingFile);
 end;
 
 procedure TSheetExporter.Save(const ADoneMessage: String;
                               const ADefaultFileName: String;
                               const AOverwriteExistingFile: Boolean = False);
-var
-  FileFormat: TsSpreadsheetFormat;
-  FileName: String;
 begin
-  FileName:=  ADefaultFileName;
-  if not OpenSaveDialog(FileName, FileFormat) then Exit;
-  SaveToFormat(FileFormat, ADoneMessage, FileName, AOverwriteExistingFile);
+  SaveWithDialog(FWorkbook, ADoneMessage,
+                 ADefaultFileName, AOverwriteExistingFile);
 end;
 
 procedure TSheetExporter.PageSettings(
@@ -157,38 +308,12 @@ procedure TSheetExporter.PageSettings(
                              const ABottomMargin: Double=10;  //mm
                              const AFooterMargin: Double=0;   //mm
                              const AHeaderMargin: Double=0);  //mm
-var
-  WSheet: TsWorksheet;
 begin
-  WSheet:= FWorkBook.ActiveWorksheet;
-  WSheet.Options:= [];
-  if AShowHeaders then
-    WSheet.Options:= WSheet.Options + [soShowHeaders];
-  if AShowGridLines then
-    WSheet.Options:= WSheet.Options + [soShowGridLines];
-  WSheet.PageLayout.Orientation:= AOrient;
-  WSheet.PageLayout.TopMargin:= ATopMargin;
-  WSheet.PageLayout.LeftMargin:= ALeftMargin;
-  WSheet.PageLayout.RightMargin:= ARightMargin;
-  WSheet.PageLayout.BottomMargin:= ABottomMargin;
-  WSheet.PageLayout.HeaderMargin:= AHeaderMargin;
-  WSheet.PageLayout.FooterMargin:= AFooterMargin;
-  WSheet.PageLayout.Options:=
-    WSheet.PageLayout.Options + [poFitPages, poHorCentered];
-  case APageFit of
-    pfOnePage: begin
-                 WSheet.PageLayout.FitWidthToPages:= 1;
-                 WSheet.PageLayout.FitHeightToPages:= 1;
-               end;
-    pfWidth  : begin
-                 WSheet.PageLayout.FitWidthToPages:= 1;
-                 WSheet.PageLayout.FitHeightToPages:= 0;
-               end;
-    pfHeight : begin
-                 WSheet.PageLayout.FitWidthToPages:= 0;
-                 WSheet.PageLayout.FitHeightToPages:= 1;
-               end;
-  end;
+  SheetPageSettings(FWorkBook.ActiveWorksheet, AOrient, APageFit,
+              AShowHeaders, AShowGridLines,
+              ATopMargin, ALeftMargin, ARightMargin, ABottomMargin,
+              AFooterMargin, AHeaderMargin);
+
 end;
 
 end.
